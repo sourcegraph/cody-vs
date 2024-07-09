4ad18d1475d20ebad1ff88573445a9f51419f7cb
Piotr Karczmarz <piotr@karczmarz.com>
2024-07-09 15:23:54 +0200

    Using VsixTesting.Xunit with the latest XUnit and XUnit.Runner.VisualStudio   - based on https://github.com/josetr/VsixTestingSamples

---
+++ src/Cody.VisualStudio.Tests/AsyncPackageTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Xunit;
+
+namespace Cody.VisualStudio.Tests
+{
+    public class AsyncPackageTests
+    {
+        [VsTheory(Version = "2022")]
+        [InlineData(CodyPackage.PackageGuidString, true)]
+        async Task LoadTestAsync(string guidString, bool expectedSuccess)
+        {
+            var shell = (IVsShell7)ServiceProvider.GlobalProvider.GetService(typeof(SVsShell));
+            Assert.NotNull(shell);
+
+            var guid = Guid.Parse(guidString);
+
+            if (expectedSuccess)
+                await shell.LoadPackageAsync(ref guid);
+            else
+                await Assert.ThrowsAnyAsync<Exception>(async () => await shell.LoadPackageAsync(ref guid));
+        }
+    }
+}
