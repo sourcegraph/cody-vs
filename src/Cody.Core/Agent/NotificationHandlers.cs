631b22bca2f69af9470d593a92a8ece71e09733f
tomaszgolebiowski <37900990+tomaszgolebiowski@users.noreply.github.com>
2024-07-23 16:34:59 +0200

    Bindings to webview agent communication (#15)
    Added bindigns for:
    - webview/registerWebviewViewProvider
    - webview/resolveWebviewView
    - webview/setOptions
    - webview/setHtml
    - webview/receiveMessageStringEncoded
    - webview/postMessageStringEncoded
    - webview/createWebviewPanel
    
    ---------
    
    Co-authored-by: Tomasz Gołębiowski <tgolebiowski@virtuslab.com>

---
+++ src/Cody.Core/Agent/NotificationHandlers.cs
@@ -0,0 +1,58 @@
+﻿using Cody.Core.Agent.Protocol;
+using Newtonsoft.Json.Linq;
+using StreamJsonRpc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cody.Core.Agent
+{
+    public class NotificationHandlers
+    {
+        [JsonRpcMethod("debug/message")]
+        public void Debug(string channel, string message)
+        {
+            System.Diagnostics.Debug.WriteLine(message, "Agent");
+        }
+
+
+        [JsonRpcMethod("webview/registerWebviewViewProvider")]
+        public void RegisterWebviewViewProvider(string viewId, bool retainContextWhenHidden)
+        {
+
+        }
+
+        [JsonRpcMethod("webview/createWebviewPanel", UseSingleObjectParameterDeserialization = true)]
+        public void CreateWebviewPanel(CreateWebviewPanelParams panelParams)
+        {
+
+        }
+
+        [JsonRpcMethod("webview/setOptions")]
+        public void SetOptions(string handle, DefiniteWebviewOptions options)
+        {
+            if(options.EnableCommandUris is bool enableCmd)
+            {
+
+            }
+            else if(options.EnableCommandUris is JArray jArray)
+            {
+                var uris = jArray.ToObject<string[]>();
+            }
+        }
+
+        [JsonRpcMethod("webview/setHtml")]
+        public void SetHtml(string handle, string html)
+        {
+
+        }
+
+        [JsonRpcMethod("webview/postMessageStringEncoded")]
+        public void PostMessageStringEncoded(string id, string stringEncodedMessage)
+        {
+
+        }
+    }
+}
---
+++ src/Cody.Core/Agent/Protocol/DefiniteWebviewOptions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cody.Core.Agent.Protocol
+{
+    public class DefiniteWebviewOptions
+    {
+        public bool EnableScripts { get; set; }
+
+        public bool EnableForms { get; set; }
+
+        public object EnableCommandUris { get; set; }
+
+        public string[] LocalResourceRoots { get; set; }
+
+        public PortMapping[] PortMapping { get; set; }
+
+        public bool EnableFindWidget { get; set; }
+
+        public bool RetainContextWhenHidden { get; set; }
+    }
+
+
+    public class PortMapping
+    {
+        public int WebviewPort { get; set; }
+
+        public int ExtensionHostPort { get; set; }
+    }
+}
