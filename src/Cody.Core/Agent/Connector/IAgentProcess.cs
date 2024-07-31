bf8f802f168f21c3b22c858b51f24cb9c5b6930c
Dominic Cooney <dominic.cooney@sourcegraph.com>
2024-07-30 18:47:47 -0700

    Connect to a remote agent.

---
+++ src/Cody.Core/Agent/Connector/IAgentProcess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cody.Core.Agent.Connector
+{
+    internal interface IAgentProcess : IDisposable
+    {
+        bool IsConnected { get; }
+        Stream SendingStream { get; }
+        Stream ReceivingStream { get; }
+    }
+}
