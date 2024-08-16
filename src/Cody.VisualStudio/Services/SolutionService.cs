6b6fcd9e0ccc732dc60f6d2825a3d365e895ff1e
tomaszgolebiowski <37900990+tomaszgolebiowski@users.noreply.github.com>
2024-08-16 12:45:31 +0200

    Remove active waiting for opeing solution (#45)
    Co-authored-by: Tomasz Gołębiowski <tgolebiowski@virtuslab.com>

--- src/Cody.VisualStudio/Services/SolutionService.cs
+++ src/Cody.VisualStudio/Services/SolutionService.cs
@@ -1,4 +1,4 @@
-﻿using Cody.Core.Infrastructure;
+using Cody.Core.Infrastructure;
 using Microsoft.VisualStudio.Shell.Interop;
 using System;
 using System.Diagnostics;
@@ -23,14 +23,6 @@ namespace Cody.VisualStudio.Services
 
         public string GetSolutionDirectory()
         {
-            // Wait for solution to finish loading
-            solutionService.GetProperty((int)__VSPROPID.VSPROPID_IsSolutionOpening, out object loading);
-            while (loading is bool isLoading && isLoading)
-            {
-                System.Threading.Thread.Sleep(100);
-                solutionService.GetProperty((int)__VSPROPID.VSPROPID_IsSolutionOpening, out loading);
-            }
-
             solutionService.GetProperty((int)__VSPROPID.VSPROPID_SolutionFileName, out object value);
             var solutionDir = Path.GetDirectoryName(value as string);
 
