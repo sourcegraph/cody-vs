f970c836f5ad0c8f1ad58a8959e6a84df882ce1a
Piotr Karczmarz <piotr@karczmarz.com>
2024-09-05 15:51:38 +0200

    Changed to localhost.

--- src/Cody.VisualStudio.Tests/PlaywrightTestsBase.cs
+++ src/Cody.VisualStudio.Tests/PlaywrightTestsBase.cs
@@ -10,7 +10,7 @@ namespace Cody.VisualStudio.Tests
 {
     public abstract class PlaywrightTestsBase: TestsBase
     {
-        protected string CdpAddress = $"http://127.0.0.1:{9222}";
+        protected string CdpAddress = $"http://localhost:{9222}";
         
         protected IPlaywright Playwright;
         protected IBrowser Browser;
