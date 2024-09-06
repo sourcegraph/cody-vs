1950186416bb03e66b6a8a3f7cbdab3d40cde914
Beatrix <beatrix@sourcegraph.com>
2024-09-06 08:28:25 -0700

    Updat eoptions page

--- src/Cody.VisualStudio/Services/OptionsPage.cs
+++ src/Cody.VisualStudio/Services/OptionsPage.cs
@@ -6,13 +6,13 @@ namespace Cody.VisualStudio
     public class OptionsPage : DialogPage
     {
         [Category("Authentication")]
-        [DisplayName("Access Token")]
-        [Description("Paste your access token. To create an access token, go to 'Settings' and then 'Access tokens' on the Sourcegraph instance.")]
-        public string TokenKey { get; set; }
-
-        [Category("Authentication")]
         [DisplayName("Sourcegraph URL")]
-        [Description("Enter the URL of the Sourcegraph instance. For example, https://sourcegraph.example.com.")]
+        [Description("The URL of the Sourcegraph instance you are connecting to.")]
         public string Endpoint { get; set; }
+
+        [Category("Configurations")]
+        [DisplayName("Extension Settings")]
+        [Description("[Current Not Supported] Your Cody extension settings in JSON format.")]
+        public string Settings { get; set; }
     }
 }
