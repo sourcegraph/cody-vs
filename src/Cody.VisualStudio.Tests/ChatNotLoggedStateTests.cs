98ae681025d2f38a409332ed625fab18b5d9fcc8
Tomasz Gołębiowski <tgolebiowski@virtuslab.com>
2024-10-01 13:46:03 +0200

    disable ChatNotLoggedStateTests

--- src/Cody.VisualStudio.Tests/ChatNotLoggedStateTests.cs
+++ src/Cody.VisualStudio.Tests/ChatNotLoggedStateTests.cs
@@ -14,34 +14,34 @@ namespace Cody.VisualStudio.Tests
         {
         }
 
-        [VsFact(Version = VsVersion.VS2022)]
-        public async Task Loads_Properly_InNotLoggedState()
-        {
-            // given
+        //[VsFact(Version = VsVersion.VS2022)]
+        //public async Task Loads_Properly_InNotLoggedState()
+        //{
+        //    // given
 
-            var text = "Cody Free or Cody Pro";
-            IReadOnlyList<string> textContents;
-            string accessToken = null;
-            try
-            {
-                await WaitForPlaywrightAsync();
+        //    var text = "Cody Free or Cody Pro";
+        //    IReadOnlyList<string> textContents;
+        //    string accessToken = null;
+        //    try
+        //    {
+        //        await WaitForPlaywrightAsync();
 
-                accessToken = await GetAccessToken();
-                if (accessToken != null)
-                    await SetAccessToken("INVALID");
+        //        accessToken = await GetAccessToken();
+        //        if (accessToken != null)
+        //            await SetAccessToken("INVALID");
 
-                // when
-                var getStarted = Page.GetByText(text);
-                textContents = await getStarted.AllTextContentsAsync();
-            }
-            finally
-            {
-                if (accessToken != null)
-                    await SetAccessToken(accessToken); // make it valid
-            }
+        //        // when
+        //        var getStarted = Page.GetByText(text);
+        //        textContents = await getStarted.AllTextContentsAsync();
+        //    }
+        //    finally
+        //    {
+        //        if (accessToken != null)
+        //            await SetAccessToken(accessToken); // make it valid
+        //    }
 
-            // then
-            Assert.Equal(text, textContents.First());
-        }
+        //    // then
+        //    Assert.Equal(text, textContents.First());
+        //}
     }
 }
