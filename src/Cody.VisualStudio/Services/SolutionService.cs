8b6747755691e2120b4c4492619c4e6c135bbedc
Piotr Karczmarz <piotr@karczmarz.com>
2024-10-14 14:42:42 +0200

    Added logging when solution (workspace) is changed. Improved logging for SolutionService.

--- src/Cody.VisualStudio/Services/SolutionService.cs
+++ src/Cody.VisualStudio/Services/SolutionService.cs
@@ -3,33 +3,40 @@ using Microsoft.VisualStudio.Shell.Interop;
 using System;
 using System.Diagnostics;
 using System.IO;
+using Cody.Core.Logging;
 
 namespace Cody.VisualStudio.Services
 {
     public class SolutionService : ISolutionService
     {
-        private IVsSolution solutionService;
+        private readonly IVsSolution _vsSolution;
+        private readonly ILog _logger;
 
-        public SolutionService(IVsSolution solutionService)
+        public SolutionService(IVsSolution solutionService, ILog logger)
         {
-            this.solutionService = solutionService;
+            _vsSolution = solutionService;
+            _logger = logger;
         }
 
         public bool IsSolutionOpen()
         {
-            solutionService.GetProperty((int)__VSPROPID.VSPROPID_IsSolutionOpen, out object value);
-            return value is bool isSolutionOpen && isSolutionOpen;
+            _vsSolution.GetProperty((int)__VSPROPID.VSPROPID_IsSolutionOpen, out object value);
+            var isOpened = value is bool isSolutionOpen && isSolutionOpen;
+
+            _logger.Debug($"Is solution opened:{isOpened}");
+
+            return isOpened;
         }
 
         public string GetSolutionDirectory()
         {
-            solutionService.GetProperty((int)__VSPROPID.VSPROPID_SolutionFileName, out object value);
+            _vsSolution.GetProperty((int)__VSPROPID.VSPROPID_SolutionFileName, out object value);
             var solutionDir = Path.GetDirectoryName(value as string);
 
             // Use user directory if solution directory is null.
             var userDir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
 
-            Debug.WriteLine(solutionDir, "GetSolutionDirectory");
+            _logger.Debug($"'{solutionDir}'");
 
             return new Uri(solutionDir ?? userDir).ToString();
         }
