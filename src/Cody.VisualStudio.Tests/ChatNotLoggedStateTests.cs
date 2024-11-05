2563b134d1870437e66f0baec8674bd5ac0a2baf
Piotr Karczmarz <piotr@karczmarz.com>
2024-11-05 15:33:58 +0100

    Introduced ThreadHelper.JoinableTaskContext

--- src/Cody.VisualStudio.Tests/ChatNotLoggedStateTests.cs
+++ src/Cody.VisualStudio.Tests/ChatNotLoggedStateTests.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.Threading;
 using Xunit;
 using Xunit.Abstractions;
+using Microsoft.VisualStudio.Shell;
 
 namespace Cody.VisualStudio.Tests
 {
     public class ChatNotLoggedStateTests : PlaywrightTestsBase, IDisposable
     {
+        private readonly JoinableTaskContext _context = ThreadHelper.JoinableTaskContext;
+
         public ChatNotLoggedStateTests(ITestOutputHelper output) : base(output)
         {
+            _context.Factory.Run(async () =>
+            {
+                await WaitForPlaywrightAsync();
+            });
         }
 
         [VsFact(Version = VsVersion.VS2022)]
@@ -24,7 +32,7 @@ namespace Cody.VisualStudio.Tests
             string accessToken = null;
             try
             {
-                await WaitForPlaywrightAsync();
+                //await WaitForPlaywrightAsync();
 
                 accessToken = await GetAccessToken();
                 if (accessToken != null)
