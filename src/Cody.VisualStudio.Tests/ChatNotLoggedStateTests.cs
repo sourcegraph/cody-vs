f727c1af4f523772da9429803c0a0adb923d9a01
Piotr Karczmarz <piotr@karczmarz.com>
2024-11-05 16:33:02 +0100

    Added test for Cody Free/Cody Enterprise sections.

--- src/Cody.VisualStudio.Tests/ChatNotLoggedStateTests.cs
+++ src/Cody.VisualStudio.Tests/ChatNotLoggedStateTests.cs
@@ -14,48 +14,72 @@ namespace Cody.VisualStudio.Tests
     {
         private readonly JoinableTaskContext _context = ThreadHelper.JoinableTaskContext;
 
+        private string _accessToken;
+
         public ChatNotLoggedStateTests(ITestOutputHelper output) : base(output)
         {
             _context.Factory.Run(async () =>
             {
                 await WaitForPlaywrightAsync();
+                _accessToken = await GetAccessToken();
+
+                if (_accessToken != null)
+                    await SetAccessToken("INVALID");
             });
+
         }
 
         [VsFact(Version = VsVersion.VS2022)]
-        public async Task Loads_Properly_InNotLoggedState()
+        public async Task Cody_Free_Cody_Pro_Section_Is_Present()
+        {
+            // given
+            var sectionText = "Cody Free or Cody Pro";
+            var buttonText = "Sign In with GitHub";
+
+            // then
+            await AssertTestIsPresent(sectionText);
+            await AssertTestIsPresent(buttonText);
+        }
+
+        [VsFact(Version = VsVersion.VS2022)]
+        public async Task Cody_Enterprise_Section_Is_Present()
+        {
+            // given
+            var sectionText = "Cody Enterprise";
+            var browserButtonText = " Sign In with Browser";
+            var tokenButtonText = " Sign In with Access Token";
+
+            // then
+            await AssertTestIsPresent(sectionText);
+            await AssertTestIsPresent(browserButtonText);
+            await AssertTestIsPresent(tokenButtonText);
+        }
+
+        private async Task AssertTestIsPresent(string text)
         {
             // given
 
-            var text = "Cody Free or Cody Pro";
-            IReadOnlyList<string> textContents;
-            string accessToken = null;
-            try
-            {
-                //await WaitForPlaywrightAsync();
-
-                accessToken = await GetAccessToken();
-                if (accessToken != null)
-                    await SetAccessToken("INVALID");
-
-                // when
-                var getStarted = Page.GetByText(text);
-                textContents = await getStarted.AllTextContentsAsync();
-            }
-            finally
-            {
-                if (accessToken != null)
-                    await SetAccessToken(accessToken); // make it valid
-            }
+            var getStarted = Page.GetByText(text);
+            var textContents = await getStarted.AllTextContentsAsync();
 
             // then
             Assert.Equal(text, textContents.First());
         }
 
-        public void Dispose()
+        public async void Dispose()
         {
-            var testName = GetTestName();
-            TakeScreenshot(testName);
+            try
+            {
+                var testName = GetTestName();
+                TakeScreenshot(testName);
+
+                if (_accessToken != null)
+                    await SetAccessToken(_accessToken); // make it valid
+            }
+            catch (Exception ex)
+            {
+                WriteLog($"Dispose() for {GetTestName()} failed.");
+            }
         }
     }
 }
