55c369c2c1de3fddf8992d227321a5f91ca2305a
Tomasz Gołębiowski <tgolebiowski@virtuslab.com>
2024-11-18 13:45:50 +0100

    Publishing with established branch naming

---
+++ .github/actions/next-version-gen/generateVersion.ps1
@@ -0,0 +1,53 @@
+param(
+	$publishType,
+    $previewInfix
+)
+
+$isPreview = $publishType -eq "Preview"
+
+$pattern = "(?<Product>\w+)-v(?<Major>\d+)\.(?<Minor>\d+)\.(?<Patch>\w+)"
+
+if ($env:GITHUB_REF_NAME -match $pattern) {
+    $product = $Matches['Product']
+    $majorVer = $Matches['Major']
+    $minorVer = $Matches['Minor']
+    $patchVer = $Matches['Patch']
+}
+else {
+	Write-Host "::error::Invalid branch name. Example valid name: vs-v0.2.x"
+    exit 1
+}
+
+if($patchVer -ne "x") {
+	Write-Host "::error::Path version number must be set to 'x'"
+    exit 1
+}
+
+if($minorVer % 2 -eq 1) {
+	Write-Host "::error::Minor version number must be even"
+    exit 1
+}
+
+if($isPreview) {
+	$newMinorVer = $minorVer - 1
+	$infix = $previewInfix
+} else {
+	$newMinorVer = $minorVer
+	$infix = ""
+}
+
+$nextPathVer = 0
+
+do {
+	$nextVersion = "$majorVer.$newMinorVer.$nextPathVer"
+	$nextVersionTag = "$product$infix-v$nextVersion"
+	git -C $env:GITHUB_WORKSPACE rev-parse "refs/tags/$nextVersionTag" --quiet 2> Out-Null
+	if($?) { $nextPathVer = $nextPathVer + 1 }
+	else { break }
+} while($true)
+
+"next-version=$nextVersion" | Out-File -FilePath $env:GITHUB_OUTPUT -Append
+"next-version-tag=$nextVersionTag" | Out-File -FilePath $env:GITHUB_OUTPUT -Append
+
+Write-Host "Next version: $nextVersion"
+Write-Host "Next version tag name: $nextVersionTag"
