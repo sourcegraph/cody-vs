ed1a82139ac615e28b4912df5f56abfc3e112da8
Tomasz Gołębiowski <tgolebiowski@virtuslab.com>
2024-12-09 16:02:32 +0100

    cooperation with IntelliSense

--- src/Cody.VisualStudio/Completions/CodyProposalSource.cs
+++ src/Cody.VisualStudio/Completions/CodyProposalSource.cs
@@ -14,6 +14,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using static Microsoft.VisualStudio.Shell.ThreadedWaitDialogHelper;
 
 namespace Cody.VisualStudio.Completions
 {
