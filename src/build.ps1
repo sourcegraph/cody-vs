c4cc7d93556e126780d67140914f0b5909cb7b00
Piotr Karczmarz <piotr@karczmarz.com>
2025-01-21 14:04:24 +0100

    Current path fixes.

--- src/build.ps1
+++ src/build.ps1
@@ -2,8 +2,9 @@ param(
 	$configuration = "Debug"
 )
 
+$current = $PSScriptRoot
 Write-Output "Building using $configuration configuration ..."
 
-& msbuild Cody.VisualStudio.Completions/Cody.VisualStudio.Completions.csproj -t:Build -restore -verbosity:minimal -property:Configuration=$configuration
+& msbuild $current/Cody.VisualStudio.Completions/Cody.VisualStudio.Completions.csproj -t:Build -restore -verbosity:minimal -property:Configuration=$configuration
 
-& msbuild Cody.sln -t:Build -restore -verbosity:minimal -property:Configuration=$configuration
+& msbuild $current/Cody.sln -t:Build -restore -verbosity:minimal -property:Configuration=$configuration
