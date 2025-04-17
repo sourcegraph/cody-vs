26272bc8516ea242d227a0760fa4082f1c739a2e
Piotr Karczmarz <piotr@karczmarz.com>
2025-04-17 15:05:19 +0200

    Fixes handling Home/Enter keys by Cody Chat when editing prompt.

---
+++ src/Cody.UI/Controls/CodyWebView2.cs
@@ -0,0 +1,13 @@
+using Microsoft.Web.WebView2.Wpf;
+using System.Windows.Input;
+
+namespace Cody.UI.Controls
+{
+    public class CodyWebView2: WebView2
+    {
+        protected override void OnPreviewKeyDown(KeyEventArgs e)
+        {
+            // prevents WebView2 from handling custom key events (like Home/Enter keys not working in the Cody Chat during prompt editing)
+        }
+    }
+}
