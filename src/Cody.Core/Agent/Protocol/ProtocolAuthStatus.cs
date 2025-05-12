c945733e37a1ec9178abaa6b84f3e8088e36a72c
Piotr Karczmarz <piotr@karczmarz.com>
2025-05-12 09:35:38 +0200

    Removed redundant JsonProperty attributes.

--- src/Cody.Core/Agent/Protocol/ProtocolAuthStatus.cs
+++ src/Cody.Core/Agent/Protocol/ProtocolAuthStatus.cs
@@ -11,17 +11,13 @@ namespace Cody.Core.Agent.Protocol
     [JsonConverter(typeof(ProtocolAuthStatusConverter))]
     public abstract class ProtocolAuthStatus
     {
-        [JsonProperty("status")]
         [JsonConverter(typeof(StringEnumConverter))]
         public StatusEnum Status { get; set; }
 
-        [JsonProperty("authenticated")]
         public bool Authenticated { get; set; }
 
-        [JsonProperty("endpoint")]
         public string Endpoint { get; set; }
 
-        [JsonProperty("pendingValidation")]
         public bool PendingValidation { get; set; }
 
         public enum StatusEnum
@@ -33,33 +29,25 @@ namespace Cody.Core.Agent.Protocol
 
     public class ProtocolAuthenticatedAuthStatus : ProtocolAuthStatus
     {
-        [JsonProperty("username")]
         public string Username { get; set; }
 
-        [JsonProperty("isFireworksTracingEnabled")]
         public bool? IsFireworksTracingEnabled { get; set; }
 
-        [JsonProperty("hasVerifiedEmail")]
         public bool? HasVerifiedEmail { get; set; }
 
-        [JsonProperty("requiresVerifiedEmail")]
         public bool? RequiresVerifiedEmail { get; set; }
 
-        [JsonProperty("primaryEmail")]
         public string PrimaryEmail { get; set; }
 
-        [JsonProperty("displayName")]
         public string DisplayName { get; set; }
 
-        [JsonProperty("avatarURL")]
         public string AvatarURL { get; set; }
 
-        [JsonProperty("organizations")] public List<OrganizationsParams> Organizations { get; set; }
+        public List<OrganizationsParams> Organizations { get; set; }
     }
 
     public class ProtocolUnauthenticatedAuthStatus : ProtocolAuthStatus
     {
-        [JsonProperty("error")]
         public AuthError Error { get; set; }
     }
 
