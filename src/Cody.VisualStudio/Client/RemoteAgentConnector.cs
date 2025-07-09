21b2c2e6c4d3fabb68527cd32eeec4ecbf63e41d
Piotr Karczmarz <piotr@karczmarz.com>
2025-07-09 15:46:24 +0200

    Added retry logic when connection to the agent via TCP port.

--- src/Cody.VisualStudio/Client/RemoteAgentConnector.cs
+++ src/Cody.VisualStudio/Client/RemoteAgentConnector.cs
@@ -1,25 +1,45 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net.Sockets;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using Cody.Core.Logging;
 
 namespace Cody.VisualStudio.Client
 {
     public class RemoteAgentConnector : IAgentConnector
     {
+        private readonly ILog _logger;
         private TcpClient client;
+
+        public RemoteAgentConnector(ILog logger)
+        {
+            _logger = logger;
+        }
 
         public event EventHandler<int> Disconnected;
         public event EventHandler<string> ErrorReceived;
 
         public void Connect(AgentClientOptions options)
         {
-            client = new TcpClient();
-            client.Connect(IPAddress.Loopback, options.RemoteAgentPort);
+            while (true)
+            {
+                try
+                {
+                    _logger.Debug($"Try to connect to port:{options.RemoteAgentPort} ...");
+
+                    client = new TcpClient();
+                    client.Connect(IPAddress.Loopback, options.RemoteAgentPort);
+                    return;
+                }
+                catch
+                {
+                    client.Close();
+                    client.Dispose();
+
+                    Thread.Sleep(TimeSpan.FromSeconds(5));
+                }
+            }
         }
 
         public void Disconnect()
