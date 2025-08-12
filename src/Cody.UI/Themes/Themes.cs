a1c0ef9873be4d9b121e99eb4547ebbace22a824
Tomasz Gołębiowski <tgolebiowski@virtuslab.com>
2025-08-12 13:18:39 +0200

    theming

---
+++ src/Cody.UI/Themes/Themes.cs
@@ -0,0 +1,142 @@
+using Microsoft.VisualStudio.PlatformUI;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Community.VisualStudio.Toolkit
+{
+    /// <summary>
+    /// A helper class that can automatically theme any XAML control or window using the VS theme properties.
+    /// </summary>
+    /// <remarks>Should only be referenced from within .xaml files.</remarks>
+    /// <example>
+    /// <code>
+    /// &lt;UserControl x:Class="MyClass"
+    /// xmlns:toolkit="clr-namespace:Community.VisualStudio.Toolkit;assembly=Community.VisualStudio.Toolkit"
+    /// toolkit:Themes.UseVsTheme="True"&gt;
+    /// &lt;/UserControl&gt;
+    /// </code>
+    /// </example>
+    public static class Themes
+    {
+        private static readonly DependencyProperty _originalBackgroundProperty = DependencyProperty.RegisterAttached("OriginalBackground", typeof(object), typeof(Themes));
+        private static readonly DependencyProperty _originalForegroundProperty = DependencyProperty.RegisterAttached("OriginalForeground", typeof(object), typeof(Themes));
+
+        private static ResourceDictionary _themeResources;
+
+        /// <summary>
+        /// The property to add to your XAML control.
+        /// </summary>
+        public static readonly DependencyProperty UseVsThemeProperty = DependencyProperty.RegisterAttached("UseVsTheme", typeof(bool), typeof(Themes), new PropertyMetadata(false, UseVsThemePropertyChanged));
+
+        /// <summary>
+        /// Sets the UseVsTheme property.
+        /// </summary>
+        public static void SetUseVsTheme(UIElement element, bool value) => element.SetValue(UseVsThemeProperty, value);
+
+        /// <summary>
+        /// Gets the UseVsTheme property from the specified element.
+        /// </summary>
+        public static bool GetUseVsTheme(UIElement element) => (bool)element.GetValue(UseVsThemeProperty);
+
+        private static void UseVsThemePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (!DesignerProperties.GetIsInDesignMode(d))
+            {
+                if (d is FrameworkElement element)
+                {
+                    if ((bool)e.NewValue)
+                    {
+                        OverrideProperty(element, Control.BackgroundProperty, _originalBackgroundProperty, ThemedDialogColors.WindowPanelBrushKey);
+                        OverrideProperty(element, Control.ForegroundProperty, _originalForegroundProperty, ThemedDialogColors.WindowPanelTextBrushKey);
+                        ThemedDialogStyleLoader.SetUseDefaultThemedDialogStyles(element, true);
+                        ImageThemingUtilities.SetThemeScrollBars(element, true);
+
+                        // Only merge the styles after the element has been initialized.
+                        // If the element hasn't been initialized yet, add an event handler
+                        // so that we can merge the styles once it has been initialized.
+                        if (!element.IsInitialized)
+                        {
+                            element.Initialized += OnElementInitialized;
+                        }
+                        else
+                        {
+                            MergeStyles(element);
+                        }
+                    }
+                    else
+                    {
+                        if (_themeResources != null)
+                        {
+                            element.Resources.MergedDictionaries.Remove(_themeResources);
+                        }
+                        ImageThemingUtilities.SetThemeScrollBars(element, null);
+                        ThemedDialogStyleLoader.SetUseDefaultThemedDialogStyles(element, false);
+                        RestoreProperty(element, Control.ForegroundProperty, _originalForegroundProperty);
+                        RestoreProperty(element, Control.BackgroundProperty, _originalBackgroundProperty);
+                    }
+                }
+            }
+        }
+
+        private static void OverrideProperty(FrameworkElement element, DependencyProperty property, DependencyProperty backup, object value)
+        {
+            if (element is Control control)
+            {
+                object original = control.ReadLocalValue(property);
+
+                if (!ReferenceEquals(value, DependencyProperty.UnsetValue))
+                {
+                    control.SetValue(backup, original);
+                }
+
+                control.SetResourceReference(property, value);
+            }
+        }
+
+        private static void RestoreProperty(FrameworkElement element, DependencyProperty property, DependencyProperty backup)
+        {
+            if (element is Control control)
+            {
+                object value = control.ReadLocalValue(backup);
+
+                if (!ReferenceEquals(value, DependencyProperty.UnsetValue))
+                {
+                    control.SetValue(property, value);
+                }
+                else
+                {
+                    control.ClearValue(property);
+                }
+
+                control.ClearValue(backup);
+            }
+        }
+
+        private static void OnElementInitialized(object sender, EventArgs args)
+        {
+            FrameworkElement element = (FrameworkElement)sender;
+            MergeStyles(element);
+            element.Initialized -= OnElementInitialized;
+        }
+
+        private static void MergeStyles(FrameworkElement element)
+        {
+            if (_themeResources is null)
+            {
+                _themeResources = new ResourceDictionary
+                {
+                    Source = ToolkitResourceKeys.ThemeResourcesUri
+                };
+            }
+
+            Collection<ResourceDictionary> dictionaries = element.Resources.MergedDictionaries;
+            if (!dictionaries.Contains(_themeResources))
+            {
+                dictionaries.Add(_themeResources);
+            }
+        }
+    }
+}
---
+++ src/Cody.UI/Themes/ToolkitResourceKeys.cs
@@ -0,0 +1,66 @@
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Community.VisualStudio.Toolkit
+{
+    /// <summary>Exposes WPF resource keys for WPF controls.</summary>
+    public static class ToolkitResourceKeys
+    {
+        private const string _prefix = "Toolkit";
+
+        /// <summary>
+        /// Gets the URI of the <see cref="ResourceDictionary"/> that contains the theme resources.
+        /// </summary>
+        /// <remarks>
+        /// This can be used to load the <see cref="ResourceDictionary"/> when you would like to customize the styles.
+        /// <code>
+        /// &lt;ResourceDictionary&gt;
+        ///     &lt;ResourceDictionary.MergedDictionaries&gt;
+        ///         &lt;ResourceDictionary Source="{x:Static toolkit:ToolkitResourceKeys.ThemeResourcesUri}"/&gt;
+        ///     &lt;/ResourceDictionary.MergedDictionaries&gt;
+        /// &lt;/ResourceDictionary&gt;
+        /// </code>
+        /// </remarks>
+        public static Uri ThemeResourcesUri { get; } = BuildPackUri("Themes/ThemeResources.xaml");
+
+        /// <summary>Gets the key that can be used to get the <see cref="Thickness"/> to use for input controls.</summary>
+        public static object InputPaddingKey { get; } = _prefix + nameof(InputPaddingKey);
+
+        /// <summary>
+        /// Gets the key that can be used to get the <see cref="double"/> to use for a progress bar with a thick height
+        /// (similar to the height used in the <see cref="IVsThreadedWaitDialog"/>).
+        /// </summary>
+        public static object ThickProgressBarHeight { get; } = _prefix + nameof(ThickProgressBarHeight);
+
+        /// <summary>Gets the key that defines the resource for a Visual Studio-themed <see cref="TextBox"/> style.</summary>
+        public static object TextBoxStyleKey { get; } = _prefix + nameof(TextBoxStyleKey);
+
+        /// <summary>Gets the key that defines the resource for a Visual Studio-themed <see cref="ComboBox"/> style.</summary>
+        public static object ComboBoxStyleKey { get; } = _prefix + nameof(ComboBoxStyleKey);
+
+        /// <summary>Gets the key that defines the resource for a Visual Studio-themed <see cref="PasswordBox"/> style.</summary>
+        public static object PasswordBoxStyleKey { get; } = _prefix + nameof(PasswordBoxStyleKey);
+
+        /// <summary>Gets the key that defines the resource for the <see cref="ControlTemplate"/> of a Visual Studio-themed <see cref="PasswordBox"/> style.</summary>
+        public static object PasswordBoxControlTemplateKey { get; } = _prefix + nameof(PasswordBoxControlTemplateKey);
+
+        /// <summary>Gets the key that defines the resource for a Visual Studio-themed <see cref="RichTextBox"/> style.</summary>
+        public static object RichTextBoxStyleKey { get; } = _prefix + nameof(RichTextBoxStyleKey);
+
+        /// <summary>Gets the key that defines the resource for the <see cref="ControlTemplate"/> of a Visual Studio-themed <see cref="RichTextBox"/> style.</summary>
+        public static object RichTextBoxControlTemplateKey { get; } = _prefix + nameof(RichTextBoxControlTemplateKey);
+
+        private static Uri BuildPackUri(string resource)
+        {
+            // Multiple versions of the toolkit assembly might be loaded, so when
+            // loading a resource, we need to include the version number of this
+            // assembly to ensure that the resource is loaded from the correct assembly.
+            AssemblyName assemblyName = typeof(ToolkitResourceKeys).Assembly.GetName();
+            string version = assemblyName.Version.ToString();
+            return new Uri($"pack://application:,,,/{assemblyName.Name};v{version};component/{resource}");
+        }
+    }
+}
